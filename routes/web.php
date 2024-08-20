67716aa3630d14dec3f6691571c13a725fb141bb
Rafal Felipe <rafaguiar2005@gmail.com>
2024-08-20 10:28:08 -0400

    feat: Adicionando controllers e views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return 'welcome';
+});*/
+
+Route::get('/',[Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [Controllers\ContatoController::class, 'contato']);
+
+//testando parametros na rota
+//parametros opcionais tem que ser passado da direita para a esquerda se não o laravel não entende
+//isso pode ser contornado criando uma logica para tratar isso
+Route::get('/contato/{nome}/{mensagem?}', 
+    function(string $nome, string $mensagem = 'nada'){
+        echo "Meu nome é $nome e quero dizer $mensagem";
+    }
+);
